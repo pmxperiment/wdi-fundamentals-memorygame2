ae293e9ca3d63f46e11f54c9d8031c6c9d1ccd7e
Patrick 1 <Patrick@Patricks-MacBook-Pro.local>
2017-11-22 23:47:10 -0800

    created game logic 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log("Up and running!");
+
+
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "King";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
